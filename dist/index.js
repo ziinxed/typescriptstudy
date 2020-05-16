e112c564a1078f5c4aa930855671fa588c5b7f60
Deok <ziinxed@gmail.com>
2020-05-16 14:17:29 +0900

    type script types

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sayHi = (name, age, gender) => {
-    return `hello ${name}, you are ${age}, you are a ${gender}`; // $ 표시를 통해 스트링에 변수 삽입
+    return `hello ${name}, you are ${age}, you are a ${gender}!`; // $ 표시를 통해 스트링에 변수 삽입
 };
 sayHi("whaing", 24, `male`);
 //# sourceMappingURL=index.js.map
