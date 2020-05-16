8ac9bd65e1b74080bf92d7bc3e66082f62bdc3a5
Deok <ziinxed@gmail.com>
2020-05-16 14:35:01 +0900

    class study

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,29 @@
 "use strict";
+/*
+interface Human { //자바스크립트로 컴파일 되지 않는다.
+    name: string;
+    age: number;
+    gender: string;
+}
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+/*
 const person = {
     name: "deok",
     age: 22,
     gender: "male"
 };
+*/
+const deok = new Human("gyu", 18, "female");
 const sayHi = (person) => {
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`; // $ 표시를 통해 스트링에 변수 삽입
 };
-console.log(sayHi(person));
+console.log(sayHi(deok));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-interface Human {
+/*
+interface Human { //자바스크립트로 컴파일 되지 않는다.
     name: string;
     age: number;
     gender: string;
 }
+*/
 
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+/*
 const person = {
     name: "deok",
     age: 22,
     gender: "male"
 };
+*/
 
+const deok = new Human("gyu", 18, "female");
 
 const sayHi = (person: Human): string => { // 함수, 파라미터의 타입을 지정
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`; // $ 표시를 통해 스트링에 변수 삽입
 
 };
 
-console.log(sayHi(person));
+console.log(sayHi(deok));
 
 export {}; //모듈로 인식 
