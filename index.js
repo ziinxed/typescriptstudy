9bf4334ba1a4e49fe1b2e4813cfc23cbff5b4f2c
Deok <ziinxed@gmail.com>
2020-05-16 13:34:35 +0900

    type script basic start

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Deok", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "Deok",
+age = 24,
+gender = "male";
+
+const sayHi = (name, age, gender?) => {//gender parameter is optional
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+
+};
+
+sayHi(name, age);
+
+export {}; //모듈로 인식
