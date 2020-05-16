a4a9b79a67fa0e4382e748d7711e9c1059046ac8
Deok <ziinxed@gmail.com>
2020-05-16 14:53:41 +0900

    Hash calcuating added

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Block {
-    constructor(index, hash, previousHash, data, timestamp) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp;
+const CryptoJS = require("crypto-js");
+let Block = /** @class */ (() => {
+    class Block {
+        constructor(index, hash, previousHash, data, timestamp) {
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+        }
     }
-}
+    Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    return Block;
+})();
 const genesisBlock = new Block(0, "20202020202020", "", "Hello", 123456);
 let blockchain = [genesisBlock];
-console.log(blockchain);
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
     public index:number;
     public hash:string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string
+        ):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(index:number,
         hash:string,
         previousHash: string,
@@ -20,8 +26,13 @@ class Block{
 
 const genesisBlock:Block = new Block(0, "20202020202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() /1000);
+
 
 export{};
